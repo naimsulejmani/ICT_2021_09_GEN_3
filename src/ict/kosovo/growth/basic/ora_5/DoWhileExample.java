20095585c8aa809bd52b599ab093f0394cbdd62e
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-09-30 20:38:16 +0200

    do while shkruaj nje numer pozitive

--- src/ict/kosovo/growth/basic/ora_5/DoWhileExample.java
+++ src/ict/kosovo/growth/basic/ora_5/DoWhileExample.java
@@ -1,7 +1,17 @@
 package ict.kosovo.growth.basic.ora_5;
 
+import java.util.Scanner;
+
 public class DoWhileExample {
     public static void main(String[] args) {
-
+        Scanner reader = new Scanner(System.in);
+        int numriPozitiv;
+        do {
+            System.out.println("Shkruaj nje numer pozitive ndash me dal prej konsole!!!");
+            numriPozitiv = reader.nextInt();
+            if (numriPozitiv <= 0)
+                System.out.println("Fatekeqesisht duhet me perserite prap!");
+        }
+        while (numriPozitiv <= 0);
     }
 }
