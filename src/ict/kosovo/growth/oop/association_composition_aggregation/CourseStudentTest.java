9712477215da73aab9f032ef09e34a5400a157e0
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-10-11 20:20:48 +0200

    example with association, aggregation and composition

--- src/ict/kosovo/growth/oop/association_composition_aggregation/CourseStudentTest.java
+++ src/ict/kosovo/growth/oop/association_composition_aggregation/CourseStudentTest.java
@@ -6,9 +6,7 @@ public class CourseStudentTest {
     public static void main(String[] args) {
         Student s1 = new Student("Aladin","Bajra", LocalDate.now().minusYears(20));
         Student s2 = new Student("Albina", "Ahmeti", LocalDate.now().minusDays(3).minusYears(18));
-
         Course java = new Course("Java and Android",1);
-
         java.enrollStudent(s1);
         java.enrollStudent(s2);
 
