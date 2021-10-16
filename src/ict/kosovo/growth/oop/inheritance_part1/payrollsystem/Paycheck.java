62c011eff6f91ac4dbfa5b1f5fab45a5fb442bd7
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-10-16 10:57:26 +0200

    payeroll sistem i kompletuar i bazuar ne fajlla (ne fakt ne nje string - textual sikurse CSV)

--- src/ict/kosovo/growth/oop/inheritance_part1/payrollsystem/Paycheck.java
+++ src/ict/kosovo/growth/oop/inheritance_part1/payrollsystem/Paycheck.java
@@ -1,4 +1,98 @@
 package ict.kosovo.growth.oop.inheritance_part1.payrollsystem;
 
+import java.time.LocalDate;
+
 public class Paycheck {
+    private Employee employee;
+    private int month;
+    private int year;
+    private LocalDate releaseDate;
+    private LocalDate insertDate;
+
+    public Paycheck(Employee employee, int month, int year, LocalDate releaseDate) {
+        this.employee = employee;
+        this.month = month;
+        this.year = year;
+        this.releaseDate = releaseDate;
+        this.insertDate = LocalDate.now();
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public LocalDate getInsertDate() {
+        return insertDate;
+    }
+
+    private String getProfession() {
+        String profession = "Puntor";
+
+        if(employee instanceof Programmer)
+            profession="Programmer";
+        else if(employee instanceof  Manager)
+            profession="Manager";
+        else if(employee instanceof Secretary)
+            profession="Secretary";
+        else if(employee instanceof Consultant)
+            profession="Consultant";
+        else if(employee instanceof SalariedEmployee)
+            profession = "Puntore with monthly salary";
+        else if(employee instanceof HourlyEmployee)
+            profession = "Puntore with hourly wages";
+
+        return profession;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Rroga per muajin: %d/%d%nPuntori: %s%nRroga: %.2f EUR%nData e leshuar: %s%nProfesioni: %s",
+                month, year,
+                employee.getId() + " " + employee.getName() + " " + employee.getSurname(),
+                employee.pay(), releaseDate, getProfession()
+        );
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
+
+
+    /*
+        Rroga per periudhen: 9/2021
+        Puntori: 1- Naim Sulejmani
+        Rroga: 100 EUR
+        Data e leshuar: data
+        Profesioni: Programer
+     */
+
 }
--- src/ict/kosovo/growth/oop/inheritance_part1/payrollsystem/PayrollGenerator.java
+++ src/ict/kosovo/growth/oop/inheritance_part1/payrollsystem/PayrollGenerator.java
@@ -1,5 +1,65 @@
 package ict.kosovo.growth.oop.inheritance_part1.payrollsystem;
 
-public class PayrollGenerator {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class PayrollGenerator {
+    public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        String fileContent =
+                "1,Naim,Sulejmani,Programmer,500;2,Filan,Fisteku,Secretary,30,10;3,Lumbardh,Gashi,Manager,1000";
+
+        String[] employeeFiles = fileContent.split(";");
+
+        for (String employeeString : employeeFiles) {
+            String[] attributes = employeeString.split(",");
+            int id = Integer.parseInt(attributes[0]);
+            String name = attributes[1];
+            String surname = attributes[2];
+            String profession = attributes[3];
+            Employee employee = null;
+
+            if (profession.equals("Programmer")) {
+                double salary = Double.parseDouble(attributes[4]);
+                employee = new Programmer(id, name, surname, salary);
+            } else if (profession.equals("Manager")) {
+                double salary = Double.parseDouble(attributes[4]);
+                employee = new Manager(id, name, surname, salary);
+            } else if (profession.equals("Secretary")) {
+                int hours = Integer.parseInt(attributes[4]);
+                double hourlyWage = Double.parseDouble(attributes[5]);
+                employee = new Secretary(id, name, surname, hours, hourlyWage);
+            } else if (profession.equals("Consultant")) {
+                int hours = Integer.parseInt(attributes[4]);
+                double hourlyWage = Double.parseDouble(attributes[5]);
+                employee = new Consultant(id, name, surname, hours, hourlyWage);
+            } else {
+                employee = new Employee(id, name, surname);
+            }
+            employees.add(employee);
+        }
+
+        int month, year;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Shkruaj muajin: ");
+        month = reader.nextInt();
+        System.out.println("Shkruaj vitin: ");
+        year = reader.nextInt();
+
+        for(Employee emp: employees) {
+            System.out.println("------------------------");
+            Paycheck paycheck = new Paycheck(emp,month,year, LocalDate.now().minusDays(1));
+            paycheck.print();
+            System.out.println();
+        }
+
+    }
 }
+
+
+
+
+
+
